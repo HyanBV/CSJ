71245234ea5d598ea4a96fa343ab56e74d026db6
Hyân Bringas <badluck4u22@hotmail.com>
2021-02-24 20:36:32 -0600

    first commit: Canadian Scientific Journal

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener("scroll", function() {
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
+
+function toggle() {
+    var header = document.querySelector("header");
+    header.classList.toggle("active");
+}
